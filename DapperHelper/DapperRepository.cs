2fc784c4c580d21a2ab8b779909ffccf802d1121
WEI.ZHOU <2324671838@qq.com>
2023-10-21 17:06:20 +0800

    升级V1.2.0.0 1.添加批量执行List SQL新方法 2.添加现有异步执行批量SQL方法 3.添加现有无事务管理执行SQL方法 4.其他优化修改

--- DapperHelper/DapperRepository.cs
+++ DapperHelper/DapperRepository.cs
@@ -16,20 +16,38 @@ namespace DapperHelper
     /// <typeparam name="TEntity">仓储实体</typeparam>
     public class DapperRepository<TEntity> : IRepository<TEntity> where TEntity : class, new()
     {
+        /// <summary>
+        /// 默认连接对象进行创建Connection对象
+        /// </summary>
         public DapperRepository()
         {
             this.Connection = DBFactory.CreateConnection();
         }
 
+        /// <summary>
+        /// 指定DBProvider进行创建Connection对象
+        /// <para>
+        /// DBProvider(MESCon,InterfaceDB,MESLogCon,MESOtherCon)
+        /// </para>
+        /// </summary>
+        /// <param name="providers"></param>
         public DapperRepository(DBProvider providers)
         {
             this.Connection = DBFactory.CreateConnection(providers);
         }
 
+        /// <summary>
+        /// 指定dbConnection进行创建Connection对象
+        /// </summary>
+        /// <param name="dbConnection"></param>
         public DapperRepository(IDbConnection dbConnection)
         {
             this.Connection = dbConnection;
         }
+
+        /// <summary>
+        /// DB Connection
+        /// </summary>
         public IDbConnection Connection
         {
             get; private set;
@@ -94,61 +112,6 @@ namespace DapperHelper
         #endregion
 
         /// <summary>
-        /// 根据主键获取实体数据
-        /// </summary>
-        /// <typeparam name="TEntity">仓储实体</typeparam>
-        /// <param name="key">主键值</param>
-        /// <returns></returns>
-        public virtual TEntity Get(object key)
-        {
-            return Get<TEntity>(key);
-        }
-
-        public TEntity Get(object key, IDbTransaction traction = null, int? commandTimeout = null)
-        {
-            return this.Get<TEntity>(key, traction, commandTimeout);
-        }
-
-        /// <summary>
-        /// 根据主键获取实体数据
-        /// </summary>
-        /// <typeparam name="TEntity">仓储实体</typeparam>
-        /// <param name="key">主键值</param>
-        /// <param name="traction">此次操作的事务对象</param>
-        /// <param name="commandTimeout">执行的超时时间</param>
-        /// <returns></returns>
-        public async Task<TEntity> GetAsync(object key, IDbTransaction traction = null, int? commandTimeout = null)
-        {
-            return await GetAsync<TEntity>(key);
-        }
-
-        /// <summary>
-        /// 根据主键获取实体数据
-        /// </summary>
-        /// <typeparam name="T">实体类型</typeparam>
-        /// <param name="key">主键值</param>
-        /// <param name="traction">此次操作的事务对象</param>
-        /// <param name="commandTimeout">执行的超时时间</param>
-        /// <returns></returns>
-        public T Get<T>(object key, IDbTransaction traction = null, int? commandTimeout = null) where T : class, new()
-        {
-            return Connection.Get<T>(key, traction, commandTimeout);
-        }
-
-        /// <summary>
-        /// 根据主键获取实体数据
-        /// </summary>
-        /// <typeparam name="T">实体类型</typeparam>
-        /// <param name="key">主键值</param>
-        /// <param name="traction">此次操作的事务对象</param>
-        /// <param name="commandTimeout">执行的超时时间</param>
-        /// <returns></returns>
-        public async Task<T> GetAsync<T>(object key, IDbTransaction traction = null, int? commandTimeout = null) where T : class, new()
-        {
-            return await Connection.GetAsync<T>(key, traction, commandTimeout);
-        }
-
-        /// <summary>
         /// Dapper 执行SQL返回DataTable类型
         /// </summary>
         /// <param name="sql"></param>
@@ -211,7 +174,7 @@ namespace DapperHelper
         /// <summary>
         /// SQL集合批量（循环）执行
         /// <para>
-        /// return [Row] 表示执行成功返回受影响行数；[-1] 表示执行异常（回退异常）；[-2]表示执行SQL集合，执行结束后，成功数不等于集合数（回退事务）
+        /// return [Row] 表示执行成功返回受影响行数；[-1] 表示执行异常（回退异常）；
         /// </para>
         /// </summary>
         /// <param name="list">SQL集合</param>
@@ -238,15 +201,8 @@ namespace DapperHelper
                             ExecutionRow += ResultRow;
                         }
                     }
-                    // 判断执行成功行数是否与SQL集合数一致，不一致表示有SQL在执行出现异常（出现异常进行事务回退）
-                    if (ExecutionRow == list.Count)
-                    {
-                        transation.Commit();
-                    } else
-                    {
-                        transation.Rollback();
-                        ExecutionRow = -2;
-                    }
+                    transation.Commit();
+                    return ExecutionRow;
                 }
                 catch (Exception ex)
                 {
@@ -277,7 +233,12 @@ namespace DapperHelper
 ";
                     foreach (string item in list)
                     {
-                        sql += item + "\r\n";
+                        string tempSql = item;
+                        if (!tempSql.EndsWith(";"))
+                        {
+                            tempSql += ";";
+                        }
+                        sql += tempSql + "\r\n";
                     }
                     sql += "end;";
 
@@ -291,6 +252,220 @@ namespace DapperHelper
                 }
             }
             return ExecutionRow;
+        }
+
+        /// <summary>
+        /// (无事务)SQL集合批量（循环）执行
+        /// <para>
+        /// return [Row] 表示执行成功返回受影响行数；执行出现异常将转抛异常
+        /// </para>
+        /// </summary>
+        /// <param name="list">SQL集合</param>
+        /// <returns>执行完成受影响的行数</returns>
+        public int BatchExecutionForeachNTS(List<string> list)
+        {
+            int ExecutionRow = 0;
+            Connection.Open();
+            try
+            {
+                foreach (var item in list)
+                {
+                    string sqlOriginal = item.Trim();
+                    // 确保SQL不以【;】结尾
+                    if (sqlOriginal.EndsWith(";") || sqlOriginal.EndsWith("；"))
+                    {
+                        sqlOriginal = sqlOriginal.Substring(0, sqlOriginal.Length - 1);
+                    }
+                    int ResultRow = this.Connection.Execute(sqlOriginal);
+                    if (ResultRow == 1)
+                    {
+                        ExecutionRow += ResultRow;
+                    }
+                }
+                return ExecutionRow;
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// (无事务)SQL集合拼接为字符串采用BeginEnd执行，SQL集合中的SQL必须以[;]结尾
+        /// <para>
+        /// return [1] 表示执行成功；执行异常将抛出异常信息
+        /// </para>
+        /// </summary>
+        /// <param name="list">SQL集合</param>
+        /// <returns>执行状态</returns>
+        public int BatchExecutionBeginEndNTS(List<string> list)
+        {
+            int ExecutionRow = 1;
+            Connection.Open();
+            try
+            {
+                string sql = $@"begin
+";
+                foreach (string item in list)
+                {
+                    string tempSql = item;
+                    if (!tempSql.EndsWith(";"))
+                    {
+                        tempSql += ";";
+                    }
+                    sql += tempSql + "\r\n";
+                }
+                sql += "end;";
+                int result = Connection.Execute(sql);
+                return ExecutionRow;
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// SQL集合批量（循环）执行
+        /// <para>
+        /// return [Row] 表示执行成功返回受影响行数；[-1] 表示执行异常（回退异常）；
+        /// </para>
+        /// </summary>
+        /// <param name="list">SQL集合</param>
+        /// <returns>执行状态</returns>
+        public async Task<int> BatchExecutionForeachAsync(List<string> list)
+        {
+            int ExecutionRow = 0;
+            Connection.Open();
+            using (var transation = this.Connection.BeginTransaction())
+            {
+                try
+                {
+                    foreach (var item in list)
+                    {
+                        string sqlOriginal = item.Trim();
+                        // 确保SQL不以【;】结尾
+                        if (sqlOriginal.EndsWith(";") || sqlOriginal.EndsWith("；"))
+                        {
+                            sqlOriginal = sqlOriginal.Substring(0, sqlOriginal.Length - 1);
+                        }
+                        int ResultRow = await Connection.ExecuteAsync(sqlOriginal, transaction: transation);
+                        ExecutionRow += ResultRow;
+                    }
+                    transation.Commit();
+                    return ExecutionRow;
+                } catch (Exception ex)
+                {
+                    transation.Rollback();
+                    ExecutionRow = -1;
+                }
+            }
+            return ExecutionRow;
+        }
+
+        /// <summary>
+        /// SQL集合拼接为字符串采用BeginEnd执行，SQL集合中的SQL必须以[;]结尾
+        /// <para>
+        /// return [1] 表示执行成功（提交事务）；[0] 表示执行异常（回退异常），出现异常可能为SQL集合中部分SQL语句错误，或者SQL结尾未加[;]
+        /// </para>
+        /// </summary>
+        /// <param name="list">SQL集合</param>
+        /// <returns>执行状态</returns>
+        public async Task<int> BatchExecutionBeginEndAsync(List<string> list)
+        {
+            int ExecutionRow = 1;
+            Connection.Open();
+            using (var transation = this.Connection.BeginTransaction())
+            {
+                try
+                {
+                    string sql = $@"begin
+";
+                    foreach (string item in list)
+                    {
+                        string tempSql = item;
+                        if (!tempSql.EndsWith(";"))
+                        {
+                            tempSql += ";";
+                        }
+                        sql += tempSql + "\r\n";
+                    }
+                    sql += "end;";
+
+                    int result = await Connection.ExecuteAsync(sql, transaction: transation);
+                    transation.Commit();
+                    return ExecutionRow;
+                } catch (Exception ex)
+                {
+                    transation.Rollback();
+                    ExecutionRow = 0;
+                }
+            }
+            return ExecutionRow;
+        }
+
+        /// <summary>
+        /// (无事务)SQL集合批量（循环）执行
+        /// <para>
+        /// return [Row] 表示执行成功返回受影响行数；执行出现异常将转抛异常
+        /// </para>
+        /// </summary>
+        /// <param name="list">SQL集合</param>
+        /// <returns>执行完成受影响的行数</returns>
+        public async Task<int> BatchExecutionForeachAsyncNTS(List<string> list)
+        {
+            int ExecutionRow = 0;
+            Connection.Open();
+            try
+            {
+                foreach (var item in list)
+                {
+                    string sqlOriginal = item.Trim();
+                    // 确保SQL不以【;】结尾
+                    if (sqlOriginal.EndsWith(";") || sqlOriginal.EndsWith("；"))
+                    {
+                        sqlOriginal = sqlOriginal.Substring(0, sqlOriginal.Length - 1);
+                    }
+                    int ResultRow = await Connection.ExecuteAsync(sqlOriginal);
+                    ExecutionRow += ResultRow;
+                }
+                return ExecutionRow;
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// (无事务)SQL集合拼接为字符串采用BeginEnd执行，SQL集合中的SQL必须以[;]结尾
+        /// <para>
+        /// return [1] 表示执行成功；执行异常将抛出异常信息
+        /// </para>
+        /// </summary>
+        /// <param name="list">SQL集合</param>
+        /// <returns>执行状态</returns>
+        public async Task<int> BatchExecutionBeginEndAsyncNTS(List<string> list)
+        {
+            int ExecutionRow = 1;
+            Connection.Open();
+            try
+            {
+                string sql = $@"begin
+";
+                foreach (string item in list)
+                {
+                    string tempSql = item;
+                    if (!tempSql.EndsWith(";"))
+                    {
+                        tempSql += ";";
+                    }
+                    sql += tempSql + "\r\n";
+                }
+                sql += "end;";
+                int result = await Connection.ExecuteAsync(sql);
+                return ExecutionRow;
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         /// <summary>
@@ -318,6 +493,161 @@ namespace DapperHelper
                 }
             }
             return ExecutionStatus;
+        }
+
+        /// <summary>
+        /// ExecuteSqlListAsync,异步批量执行Sql List
+        /// </summary>
+        /// <param name="sqlList">SQLS对象集合</param>
+        /// <returns></returns>
+        public async Task ExecuteSqlListAsync(IEnumerable<string> sqlList)
+        {
+            Connection.Open();
+            using (var transaction = Connection.BeginTransaction())
+            {
+                try
+                {
+                    foreach (var sql in sqlList)
+                    {
+                        await Connection.ExecuteAsync(sql, transaction: transaction);
+                    }
+
+                    transaction.Commit();
+                } catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }           
+        }
+
+        /// <summary>
+        /// ExecuteSqlListAsyncResultRows,异步批量执行Sql List
+        /// </summary>
+        /// <param name="sqlList">SQLS对象集合</param>
+        /// <returns>执行结果</returns>
+        public async Task<int> ExecuteSqlListAsyncResultRows(IEnumerable<string> sqlList)
+        {
+            int ResultRows = 0;
+            Connection.Open();
+            using (var transaction = Connection.BeginTransaction())
+            {
+                try
+                {
+                    foreach (var sql in sqlList)
+                    {
+                        int status = await Connection.ExecuteAsync(sql, transaction: transaction);
+                        ResultRows += status;
+                    }
+                    transaction.Commit();
+                    return ResultRows;
+                } catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// (无事务)ExecuteSqlListAsync,异步批量执行Sql List，取消事务管理
+        /// </summary>
+        /// <param name="sqlList">SQLS对象集合</param>
+        /// <returns>执行异常会抛出对应错误异常</returns>
+        public async Task ExecuteSqlListAsyncNTS(IEnumerable<string> sqlList)
+        {
+            Connection.Open();
+            try
+            {
+                foreach (var sql in sqlList)
+                {
+                     await Connection.ExecuteAsync(sql);
+                }
+            } catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// (无事务)ExecuteSqlListAsyncResultRowsNTS,异步批量执行Sql List，取消事务管理，执行成功返回受影响的行数
+        /// </summary>
+        /// <param name="sqlList">SQLS对象集合</param>
+        /// <returns>执行成功返回受影响的行数，执行异常会抛出对应错误异常</returns>
+        public async Task<int> ExecuteSqlListAsyncResultRowsNTS(IEnumerable<string> sqlList)
+        {
+            int resultRows = 0;
+            Connection.Open();
+            try
+            {
+                foreach (var sql in sqlList)
+                {
+                    int status = await Connection.ExecuteAsync(sql);
+                    resultRows += status;
+                }
+                return resultRows;
+            } catch
+            {
+                throw;
+            }
+        }
+
+        
+
+
+        /// <summary>
+        /// 根据主键获取实体数据
+        /// </summary>
+        /// <typeparam name="TEntity">仓储实体</typeparam>
+        /// <param name="key">主键值</param>
+        /// <returns></returns>
+        public virtual TEntity Get(object key)
+        {
+            return Get<TEntity>(key);
+        }
+
+        public TEntity Get(object key, IDbTransaction traction = null, int? commandTimeout = null)
+        {
+            return this.Get<TEntity>(key, traction, commandTimeout);
+        }
+
+        /// <summary>
+        /// 根据主键获取实体数据
+        /// </summary>
+        /// <typeparam name="TEntity">仓储实体</typeparam>
+        /// <param name="key">主键值</param>
+        /// <param name="traction">此次操作的事务对象</param>
+        /// <param name="commandTimeout">执行的超时时间</param>
+        /// <returns></returns>
+        public async Task<TEntity> GetAsync(object key, IDbTransaction traction = null, int? commandTimeout = null)
+        {
+            return await GetAsync<TEntity>(key);
+        }
+
+        /// <summary>
+        /// 根据主键获取实体数据
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="key">主键值</param>
+        /// <param name="traction">此次操作的事务对象</param>
+        /// <param name="commandTimeout">执行的超时时间</param>
+        /// <returns></returns>
+        public T Get<T>(object key, IDbTransaction traction = null, int? commandTimeout = null) where T : class, new()
+        {
+            return Connection.Get<T>(key, traction, commandTimeout);
+        }
+
+        /// <summary>
+        /// 根据主键获取实体数据
+        /// </summary>
+        /// <typeparam name="T">实体类型</typeparam>
+        /// <param name="key">主键值</param>
+        /// <param name="traction">此次操作的事务对象</param>
+        /// <param name="commandTimeout">执行的超时时间</param>
+        /// <returns></returns>
+        public async Task<T> GetAsync<T>(object key, IDbTransaction traction = null, int? commandTimeout = null) where T : class, new()
+        {
+            return await Connection.GetAsync<T>(key, traction, commandTimeout);
         }
 
         public IEnumerable<TEntity> GetAll()
