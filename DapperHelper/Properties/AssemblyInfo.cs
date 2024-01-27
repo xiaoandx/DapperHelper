0a9aef8d054e16119138c6b4b0d4cd95811b7a9e
WEI.ZHOU <2324671838@qq.com>
2024-01-27 17:33:58 +0800

    1.2.2 Release improve dll description

--- DapperHelper/Properties/AssemblyInfo.cs
+++ DapperHelper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("DapperHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DapperHelper.Orcale")]
+[assembly: AssemblyDescription("DapperHelperdll is compatible with the Oracle database, while other databases are not compatible")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DapperHelper")]
+[assembly: AssemblyCompany("Individual developer WEI.ZHOU")]
+[assembly: AssemblyProduct("DapperHelper.Orcale")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.2.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
